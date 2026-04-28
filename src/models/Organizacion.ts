d28373a931ab91c9ddfbdc8c526d539c61890f0a
Wenjie Chen <wenjie.sab@gmail.com>
2026-04-28 10:51:19 +0100

    Se ha agregado virtuals

--- src/models/Organizacion.ts
+++ src/models/Organizacion.ts
@@ -2,7 +2,8 @@ import mongoose, { Document, Schema, Types } from 'mongoose';
 
 export interface IOrganizacion {
     name: string;
-    usuarios: Types.ObjectId[] | string[];
+    // por que pones que es una relacion n..1 directa??? en el modelo usuario ya tenemos un campo organizacion, con un lookup reverso ya tenemos suficiente.
+    //usuarios: Types.ObjectId[] | string[];
 }
 
 export interface IOrganizacionModel extends IOrganizacion, Document {}
@@ -10,11 +11,21 @@ export interface IOrganizacionModel extends IOrganizacion, Document {}
 const OrganizacionSchema: Schema = new Schema(
     {
         name: { type: String, required: true },
-        usuarios: [{ type: Schema.Types.ObjectId, ref: 'Usuario' }]
+        //usuarios: [{ type: Schema.Types.ObjectId, ref: 'Usuario' }]
     },
     {
-        versionKey: false
+        versionKey: false,
+	// Virtuales, para que haga una reverse lookup
+	toJSON: {virtuals: true},
+	toObject: {virtuals: true}
     }
 );
+
+OrganizacionSchema.virtual('usuarios', {
+	ref: 'Usuario',
+	localField: '_id',
+	foreignField: 'organizacion',
+	justOne: false
+});
 
 export default mongoose.model<IOrganizacionModel>('Organizacion', OrganizacionSchema);
